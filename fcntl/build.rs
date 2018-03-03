d337caafe652b94194f9663908845127c2572407
Jeremy Soller <jackpot51@gmail.com>
2018-03-03 09:19:25 -0700

    Remove std from outputted libc.a

--- fcntl/build.rs
+++ fcntl/build.rs
@@ -1,11 +1,11 @@
 extern crate cbindgen;
 
-use std::env;
+use std::{env, fs};
 
 fn main() {
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set");
+    fs::create_dir_all("../target/include").expect("failed to create include directory");
     cbindgen::generate(crate_dir)
-      .expect("Unable to generate bindings")
-      .write_to_file("../target/fcntl.h");
+      .expect("failed to generate bindings")
+      .write_to_file("../target/include/fcntl.h");
 }
