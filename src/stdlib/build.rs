083fd72e6606be20c52debf2cce412327b70f877
Jeremy Soller <jackpot51@gmail.com>
2018-03-03 20:24:40 -0700

    Reorganize

--- src/stdlib/build.rs
+++ src/stdlib/build.rs
@@ -4,8 +4,8 @@ use std::{env, fs};
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set");
-    fs::create_dir_all("../target/include").expect("failed to create include directory");
+    fs::create_dir_all("../../target/include").expect("failed to create include directory");
     cbindgen::generate(crate_dir)
       .expect("failed to generate bindings")
-      .write_to_file("../target/include/string.h");
+      .write_to_file("../../target/include/stdlib.h");
 }
