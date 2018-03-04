d9d7440a7ffe5e4c68611ec370bc784866d08cc6
Paul Sajna <paulsajna@gmail.com>
2018-03-04 06:37:45 -0800

    implement some unistd

--- platform/src/redox/mod.rs
+++ platform/src/redox/mod.rs
@@ -1,3 +1,24 @@
+use core::slice;
+
+pub unsafe fn cstr_to_slice<'a>(buf: *const c_char) -> &'a [u8] {
+    slice::from_raw_parts(buf as *const u8, ::strlen(buf) as usize)
+}
+
+pub fn chdir(path: *const c_char) -> c_int {
+    syscall::chdir(cstr_to_slice(path))? as c_int
+}
+
+pub fn close(fildes: c_int) -> c_int {
+    syscall::close(fildes as usize)? as c_int
+}
+
+pub fn dup(fildes: c_int) -> c_int {
+    syscall::dup(file as usize, &[])? as c_int
+}
+
+pub fn dup2(fildes: c_int, fildes2) -> c_int {
+    syscall::dup2(fildes as usize, fildes2 as usize, &[])? as c_int
+
 pub fn exit(status: c_int) -> ! {
     syscall::exit(status);
 }
