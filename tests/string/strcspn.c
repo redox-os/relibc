b2d01a67f28b562bc1981cd8efc9b0217b81f20d
Tom Almeida <tommoa256@gmail.com>
2018-03-14 10:55:01 +0800

    Actual working tests on strspn and strcspn

--- tests/string/strcspn.c
+++ tests/string/strcspn.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-	printf("%ld\n", strcspn("hello", "world")); // should be 2
-	printf("%ld\n", strcspn("banana", "world")); // should be 6
+	char *world = "world";
+	printf("%ld\n", strcspn("hello", world)); // should be 2
+	printf("%ld\n", strcspn("banana", world)); // should be 6
 
     return 0;
 }
