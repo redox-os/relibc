c568ca2932c0bd8a395fec516c49781b515b4776
Paul Sajna <paulsajna@gmail.com>
2018-03-15 16:18:35 -0700

    test cleanup

--- tests/waitpid.c
+++ tests/waitpid.c
@@ -5,12 +5,13 @@
 int main(int argc, char** argv) {
     pid_t pid = fork();
     if (pid == 0) {
-    // child
-    sleep(1);
-    exit(0);
-} else {
-    // parent
-    int stat_loc;
-    waitpid(pid, &stat_loc, 0);
+        // child
+        sleep(1);
+        exit(0);
+    } else {
+        // parent
+        int stat_loc;
+        waitpid(pid, &stat_loc, 0);
     }
+    return 0;
 }
