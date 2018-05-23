285d1b05c46585746c2abf9189c9cd3a252eed63
stratact <stratact1@gmail.com>
2018-05-23 11:05:43 -0700

    Implement `RawFile::open()` and add `*_raw_df()` convenience methods

--- src/platform/src/rawfile.rs
+++ src/platform/src/rawfile.rs
@@ -4,8 +4,11 @@ use sys::{open, dup, close};
 pub struct RawFile(usize);
 
 impl RawFile {
-    pub fn open<T: AsRef<[u8]>>(path: T, flags: usize) -> Result<RawFile, ()> {
-        open(path, flags).map(RawFile)
+    pub fn open<T: AsRef<[u8]>>(path: T, flags: usize, mode: usize) -> Result<RawFile, ()> {
+        match open(path.as_ref()[0] as *const i8, flags as i32, mode as u16) {
+            0 => Err(()),
+            n => Ok(RawFile(n as usize))
+        }
     }
 
     pub fn dup(&self, _buf: &[u8]) -> Result<RawFile, ()> {
@@ -13,6 +16,14 @@ impl RawFile {
             0 => Err(()),
             n => Ok(RawFile(n as usize))
         }
+    }
+
+    pub fn as_raw_fd(&self) -> usize {
+        self.0
+    }
+
+    pub fn into_raw_fd(self) -> usize {
+        self.0
     }
 }
 
