90c6937f173e8eedb1824e262b1edb9fe76a3346
Tom Almeida <tommoa256@gmail.com>
2018-06-21 00:16:20 +0800

    Changed FILE to use a vector as a buffer instead of raw pointers. This allows us to remove the large majority of unsafe blocks from the code

--- src/stdio/src/helpers.rs
+++ src/stdio/src/helpers.rs
@@ -1,6 +1,4 @@
-use super::{internal, BUFSIZ, FILE, UNGET};
-use stdlib::calloc;
-use core::{mem, ptr};
+use super::{BUFSIZ, FILE, UNGET};
 use core::sync::atomic::AtomicBool;
 use platform::types::*;
 use super::constants::*;
@@ -38,11 +36,11 @@ pub unsafe fn parse_mode_flags(mode_str: *const c_char) -> i32 {
 }
 
 /// Open a file with the file descriptor `fd` in the mode `mode`
-pub unsafe fn _fdopen(fd: c_int, mode: *const c_char) -> *mut FILE {
+pub unsafe fn _fdopen(fd: c_int, mode: *const c_char) -> Option<FILE> {
     use string::strchr;
     if *mode != b'r' as i8 && *mode != b'w' as i8 && *mode != b'a' as i8 {
         platform::errno = errno::EINVAL;
-        return ptr::null_mut();
+        return None;
     }
 
     let mut flags = 0;
@@ -62,23 +60,17 @@ pub unsafe fn _fdopen(fd: c_int, mode: *const c_char) -> *mut FILE {
         flags |= F_APP;
     }
 
-    let file = calloc(mem::size_of::<FILE>() + BUFSIZ + UNGET, 1) as *mut FILE;
     // Allocate the file
-    (*file) = FILE {
+    Some(FILE {
         flags: flags,
-        rpos: ptr::null_mut(),
-        rend: ptr::null_mut(),
-        wend: ptr::null_mut(),
-        wpos: ptr::null_mut(),
-        wbase: ptr::null_mut(),
+        read: None,
+        write: None,
         fd: fd,
-        buf: (file as *mut u8).add(mem::size_of::<FILE>() + UNGET),
-        buf_size: BUFSIZ,
+        buf: vec![0u8; BUFSIZ + UNGET],
         buf_char: -1,
         unget: UNGET,
         lock: AtomicBool::new(false),
-    };
-    file
+    })
 }
 
 /// Write buffer `buf` of length `l` into `stream`
@@ -90,58 +82,68 @@ pub fn fwritex(buf: *const u8, l: size_t, stream: &mut FILE) -> size_t {
     let mut l = l;
     let mut advance = 0;
 
-    if stream.wend.is_null() && !stream.can_write() {
+    if stream.write.is_none() && !stream.can_write() {
         // We can't write to this stream
         return 0;
     }
-    if l > stream.wend as usize - stream.wpos as usize {
-        // We can't fit all of buf in the buffer
-        return stream.write(buf);
-    }
+    if let Some((wbase, wpos, wend)) = stream.write {
+        if l > wend - wpos {
+            // We can't fit all of buf in the buffer
+            return stream.write(buf);
+        }
 
-    let i = if stream.buf_char >= 0 {
-        let mut i = l;
-        while i > 0 && buf[i - 1] != b'\n' {
-            i -= 1;
-        }
-        if i > 0 {
-            let n = stream.write(buf);
-            if n < i {
-                return n;
+        let i = if stream.buf_char >= 0 {
+            let mut i = l;
+            while i > 0 && buf[i - 1] != b'\n' {
+                i -= 1;
             }
-            advance += i;
-            l -= i;
+            if i > 0 {
+                let n = stream.write(buf);
+                if n < i {
+                    return n;
+                }
+                advance += i;
+                l -= i;
+            }
+            i
+        } else {
+            0
+        };
+
+        unsafe {
+            copy_nonoverlapping(
+                &buf[advance..] as *const _ as *const u8,
+                &mut stream.buf[wpos..] as *mut _ as *mut u8,
+                l,
+            );
         }
-        i
+        stream.write = Some((wbase, wpos + l, wend));
+        l + i
     } else {
         0
-    };
-
-    unsafe {
-        // Copy and reposition
-        copy_nonoverlapping(&buf[advance..] as *const _ as *const u8, stream.wpos, l);
-        stream.wpos = stream.wpos.add(l);
     }
-    l + i
 }
 
 /// Flush `stream` without locking it.
 pub fn fflush_unlocked(stream: &mut FILE) -> c_int {
-    if stream.wpos > stream.wbase {
-        stream.write(&[]);
-        if stream.wpos.is_null() {
+    if let Some((wbase, wpos, _)) = stream.write {
+        if wpos > wbase {
+            stream.write(&[]);
+            /*
+            if stream.wpos.is_null() {
             return -1;
+        }
+             */
         }
     }
 
-    if stream.rpos < stream.rend {
-        stream.seek(stream.rpos as i64 - stream.rend as i64, SEEK_CUR);
+    if let Some((rpos, rend)) = stream.read {
+        if rpos < rend {
+            stream.seek(rpos as i64 - rend as i64, SEEK_CUR);
+        }
     }
 
-    stream.wpos = ptr::null_mut();
-    stream.wend = ptr::null_mut();
-    stream.wbase = ptr::null_mut();
-    stream.rpos = ptr::null_mut();
-    stream.rend = ptr::null_mut();
+    stream.write = None;
+    stream.read = None;
     0
 }
