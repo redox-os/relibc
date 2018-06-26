727324fd7342140e6fc2e959c3898991d74f0c24
jD91mZM2 <me@krake.one>
2018-06-26 16:54:31 +0200

    Apparently cbindgen works on constants

--- src/signal/src/lib.rs
+++ src/signal/src/lib.rs
@@ -12,9 +12,9 @@ pub mod sys;
 #[path = "redox.rs"]
 pub mod sys;
 
-#[no_mangle] pub static SIG_BLOCK: c_int = 0;
-#[no_mangle] pub static SIG_UNBLOCK: c_int = 1;
-#[no_mangle] pub static SIG_SETMASK: c_int = 2;
+pub const SIG_BLOCK:   c_int = 0;
+pub const SIG_UNBLOCK: c_int = 1;
+pub const SIG_SETMASK: c_int = 2;
 
 pub use sys::*;
 
