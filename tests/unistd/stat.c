8021ade2a9ac6aa864bf26cc8163d372e6e49fa4
jD91mZM2 <me@krake.one>
2018-07-23 21:41:19 +0200

    Move stat test out of expected tests

--- tests/unistd/stat.c
+++ tests/unistd/stat.c
@@ -13,7 +13,12 @@ int main() {
         return 1;
     }
 
-    printf("st_mode: %u\n", buf.st_mode);
     printf("st_size: %lu\n", buf.st_size);
     printf("st_blksize: %lu\n", buf.st_blksize);
+    printf("st_dev: %lu\n", buf.st_dev);
+    printf("st_ino: %lu\n", buf.st_ino);
+    printf("st_mode: %o\n", buf.st_mode);
+    printf("st_nlink: %lu\n", buf.st_nlink);
+    printf("st_uid: %u\n", buf.st_uid);
+    printf("st_gid: %u\n", buf.st_gid);
 }
