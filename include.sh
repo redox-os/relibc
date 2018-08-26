6418f893e44bc2c9a26405865a2f2ff80a62e76a
Jeremy Soller <jeremy@system76.com>
2018-08-26 12:40:19 -0600

    Fix headers with directories

--- include.sh
+++ include.sh
@@ -9,8 +9,12 @@ for config in src/header/*/cbindgen.toml
 do
     dir="$(dirname "$config")"
     name="$(basename "$dir")"
-    pushd "$dir"
-    cargo run --release --manifest-path "$cbindgen/Cargo.toml" -- \
-        -c cbindgen.toml -o "$include/$name.h" mod.rs
-    popd
+    if [ "${name:0:1}" != "_" ]
+    then
+        header="$include/${name/_/\/}.h"
+        pushd "$dir"
+        cargo run --release --manifest-path "$cbindgen/Cargo.toml" -- \
+            -c cbindgen.toml -o "$header" mod.rs
+        popd
+    fi
 done
