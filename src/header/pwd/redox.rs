29b4c19d6ef5d7124d370173eb596fe7b75d655e
jD91mZM2 <me@krake.one>
2018-09-21 13:13:17 +0200

    fixup! Untested fix for pwd.h on redox

--- src/header/pwd/redox.rs
+++ src/header/pwd/redox.rs
@@ -4,12 +4,8 @@ pub fn split(line: &[u8]) -> [&[u8]; 7] {
 
     parts[0] = iter.next().unwrap_or(&[]);
     // Skip passwd
-    for i in 0..2 {
-        parts[2 + i] = iter.next().unwrap_or(&[]);
-    }
-    // Skip gecos
-    for i in 0..2 {
-        parts[5 + i] = iter.next().unwrap_or(&[]);
+    for i in 2..7 {
+        parts[i] = iter.next().unwrap_or(&[]);
     }
     parts
 }
