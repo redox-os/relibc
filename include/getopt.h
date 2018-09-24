b309cd832d7ccbe3fd48734cf36cbfd0ad4b5431
Jeremy Soller <jeremy@system76.com>
2018-09-24 14:45:36 -0700

    Add getopt and machine/endian.h

---
+++ include/getopt.h
@@ -0,0 +1,13 @@
+#ifndef _GETOPT_H
+#define _GETOPT_H
+
+// Generated from:
+// `grep "opt" target/include/unistd.h`
+
+extern char* optarg;
+extern int opterr;
+extern int optind;
+extern int optopt;
+int getopt(int argc, char *const *argv, const char *optstring);
+
+#endif
