0469c0c2c6e82390ea59ad9d124ab10ee1cc9cdf
jD91mZM2 <me@krake.one>
2018-10-09 16:47:57 +0200

    Add tests for memcmp and fix a teeny tiny bug

--- tests/string/mem.c
+++ tests/string/mem.c
@@ -3,22 +3,40 @@
 #include <string.h>
 
 int main(int argc, char ** argv) {
-	printf("# mem #\n");
-	char arr[100];
-	memset(arr, 0, 100); // Compiler builtin, should work
-	arr[50] = 1;
-	if ((size_t)memchr((void *)arr, 1, 100) - (size_t)arr != 50) {
-		printf("Incorrect memchr\n");
-		exit(1);
-	}
-	printf("Correct memchr\n");
-	char arr2[51];
-	memset(arr2, 0, 51); // Compiler builtin, should work
-	memccpy((void *)arr2, (void *)arr, 1, 100);
-	if (arr[50] != 1) {
-		printf("Incorrect memccpy\n");
-		exit(1);
-	}
-	printf("Correct memccpy\n");
+    puts("# mem #");
+    char arr[100];
+    memset(arr, 0, 100); // Compiler builtin, should work
+    arr[50] = 1;
+    if ((size_t)memchr((void *)arr, 1, 100) - (size_t)arr != 50) {
+        puts("Incorrect memchr");
+        exit(1);
+    }
+    puts("Correct memchr");
+    char arr2[51];
+    memset(arr2, 0, 51); // Compiler builtin, should work
+    memccpy((void *)arr2, (void *)arr, 1, 100);
+    if (arr[50] != 1) {
+        puts("Incorrect memccpy");
+        exit(1);
+    }
+    puts("Correct memccpy");
+    int res;
+    if ((res = memcmp("hello world", "hello world", 11))) {
+        printf("Incorrect memcmp (1), expected 0 found %d\n", res);
+        exit(1);
+    }
+    if ((res = memcmp("hello world", "hello worlt", 11)) >= 0) {
+        printf("Incorrect memcmp (2), expected -, found %d\n", res);
+        exit(1);
+    }
+    if ((res = memcmp("hello world", "hallo world", 5)) <= 0) {
+        printf("Incorrect memcmp (3), expected +, found %d\n", res);
+        exit(1);
+    }
+    if ((res = memcmp("hello world", "henlo world", 5)) >= 0) {
+        printf("Incorrect memcmp (4), expected -, found %d\n", res);
+        exit(1);
+    }
+    puts("Correct memcmp");
     return 0;
 }
