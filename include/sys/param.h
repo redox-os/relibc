44f84f406f08d1b838abbe7a1ae3513a29a752d4
Jeremy Soller <jeremy@system76.com>
2018-11-17 09:19:35 -0700

    Force byte order to little endian, for gcc

--- include/sys/param.h
+++ include/sys/param.h
@@ -31,4 +31,18 @@
 #include <sys/resource.h>
 #include <limits.h>
 
+/* TODO: Forcing little endian, if you need a big endian system, fix this { */
+#ifndef BIG_ENDIAN
+#define BIG_ENDIAN 4321
+#endif
+
+#ifndef LITTLE_ENDIAN
+#define LITTLE_ENDIAN 1234
+#endif
+
+#ifndef BYTE_ORDER
+#define BYTE_ORDER LITTLE_ENDIAN
+#endif
+/* } */
+
 #endif
