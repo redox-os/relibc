fa2c6d29db499c216ac00053ca40973643a6959e
Tibor Nagy <xnagytibor@gmail.com>
2019-02-25 19:32:20 +0100

    tests: Rewrite libgen tests based on ctype (nice table layout), fix error handling of sleep tests

--- tests/unistd/sleep.c
+++ tests/unistd/sleep.c
@@ -5,11 +5,16 @@
 #include "test_helpers.h"
 
 int main(void) {
-    sleep(2);
-    perror("sleep");
-    usleep(1000);
-    perror("usleep");
+    // sleep has no error codes and doesn't set errno
+    unsigned int unslept = sleep(2);
+    printf("unslept: %u\n", unslept);
+
+    int us_status = usleep(1000);
+    ERROR_IF(usleep, us_status, == -1);
+    UNEXP_IF(usleep, us_status, != 0);
+
     struct timespec tm = {0, 10000};
-    nanosleep(&tm, NULL);
-    perror("nanosleep");
+    int ns_status = nanosleep(&tm, NULL);
+    ERROR_IF(nanosleep, ns_status, == -1);
+    UNEXP_IF(nanosleep, ns_status, != 0);
 }
