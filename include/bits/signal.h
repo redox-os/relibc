e7d19e2a58f08c432d2e90f025ba019e6ee7edcf
Jeremy Soller <jeremy@system76.com>
2019-08-08 20:06:38 -0600

    Move SIG_IGN and friends to C in order to define them correctly

---
+++ include/bits/signal.h
@@ -0,0 +1,8 @@
+#ifndef _BITS_SIGNAL_H
+#define _BITS_SIGNAL_H
+
+#define SIG_DFL ((void (*)(int))0)
+#define SIG_IGN ((void (*)(int))1)
+#define SIG_ERR ((void (*)(int))-1)
+
+#endif // _BITS_SIGNAL_H
--- src/header/signal/mod.rs
+++ src/header/signal/mod.rs
@@ -21,9 +21,9 @@ pub mod sys;
 
 type SigSet = BitSet<[c_ulong; 1]>;
 
-pub const SIG_DFL: usize = 0;
-pub const SIG_IGN: usize = 1;
-pub const SIG_ERR: isize = -1;
+pub (crate) const SIG_DFL: usize = 0;
+pub (crate) const SIG_IGN: usize = 1;
+pub (crate) const SIG_ERR: isize = -1;
 
 pub const SIG_BLOCK: c_int = 0;
 pub const SIG_UNBLOCK: c_int = 1;
