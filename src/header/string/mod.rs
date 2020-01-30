16434526e27b2390f0d6b68d3a04e0cb9ac48721
Matija Skala <mskala@gmx.com>
2020-01-30 19:07:34 +0100

    add unbounded version of memchr

--- src/header/string/mod.rs
+++ src/header/string/mod.rs
@@ -129,16 +129,178 @@ pub unsafe extern "C" fn memset(s: *mut c_void, c: c_int, n: size_t) -> *mut c_v
     s
 }
 
+#[target_feature(enable = "sse2")]
+unsafe fn rawmemchr2(n1: u8, n2: u8, haystack: *const u8) -> usize {
+    use core::arch::x86_64::*;
+
+    unsafe fn forward_search2(
+        start_ptr: *const u8,
+        ptr: *const u8,
+        vn1: __m128i,
+        vn2: __m128i,
+    ) -> Option<usize> {
+        debug_assert!(start_ptr <= ptr);
+
+        let chunk = _mm_loadu_si128(ptr as *const __m128i);
+        let eq1 = _mm_cmpeq_epi8(chunk, vn1);
+        let eq2 = _mm_cmpeq_epi8(chunk, vn2);
+        if _mm_movemask_epi8(_mm_or_si128(eq1, eq2)) != 0 {
+            let mask1 = _mm_movemask_epi8(eq1);
+            let mask2 = _mm_movemask_epi8(eq2);
+            Some((ptr as usize - start_ptr as usize) + forward_pos2(mask1, mask2))
+        } else {
+            None
+        }
+    }
+
+    fn forward_pos2(mask1: i32, mask2: i32) -> usize {
+        debug_assert!(mask1 != 0 || mask2 != 0);
+
+        (mask1 | mask2).trailing_zeros() as usize
+    }
+
+    const VECTOR_SIZE: usize = mem::size_of::<__m128i>();
+    const VECTOR_ALIGN: usize = VECTOR_SIZE - 1;
+    const LOOP_SIZE2: usize = 2 * VECTOR_SIZE;
+
+    let vn1 = _mm_set1_epi8(n1 as i8);
+    let vn2 = _mm_set1_epi8(n2 as i8);
+    let loop_size = LOOP_SIZE2;
+    let start_ptr = haystack;
+    let mut ptr = start_ptr;
+
+    if let Some(i) = forward_search2(start_ptr, ptr, vn1, vn2) {
+        return i;
+    }
+
+    ptr = ptr.add(VECTOR_SIZE - (start_ptr as usize & VECTOR_ALIGN));
+    debug_assert!(ptr > start_ptr);
+    while loop_size == LOOP_SIZE2 {
+        debug_assert_eq!(0, (ptr as usize) % VECTOR_SIZE);
+
+        let a = _mm_load_si128(ptr as *const __m128i);
+        let b = _mm_load_si128(ptr.add(VECTOR_SIZE) as *const __m128i);
+        let eqa1 = _mm_cmpeq_epi8(vn1, a);
+        let eqb1 = _mm_cmpeq_epi8(vn1, b);
+        let eqa2 = _mm_cmpeq_epi8(vn2, a);
+        let eqb2 = _mm_cmpeq_epi8(vn2, b);
+        let or1 = _mm_or_si128(eqa1, eqb1);
+        let or2 = _mm_or_si128(eqa2, eqb2);
+        let or3 = _mm_or_si128(or1, or2);
+        if _mm_movemask_epi8(or3) != 0 {
+            let mut at = (ptr as usize - start_ptr as usize);
+            let mask1 = _mm_movemask_epi8(eqa1);
+            let mask2 = _mm_movemask_epi8(eqa2);
+            if mask1 != 0 || mask2 != 0 {
+                return at + forward_pos2(mask1, mask2);
+            }
+
+            at += VECTOR_SIZE;
+            let mask1 = _mm_movemask_epi8(eqb1);
+            let mask2 = _mm_movemask_epi8(eqb2);
+            return at + forward_pos2(mask1, mask2);
+        }
+        ptr = ptr.add(loop_size);
+    }
+    loop {
+        if let Some(i) = forward_search2(start_ptr, ptr, vn1, vn2) {
+            return i;
+        }
+        ptr = ptr.add(VECTOR_SIZE);
+    }
+}
+
+#[target_feature(disable = "sse2")]
+fn rawmemchr2(n1: u8, n2: u8, haystack: *const u8) -> usize {
+    #[inline(always)]
+    fn contains_zero_byte(x: usize) -> bool {
+        const LO_U64: u64 = 0x0101010101010101;
+        const HI_U64: u64 = 0x8080808080808080;
+
+        const LO_USIZE: usize = LO_U64 as usize;
+        const HI_USIZE: usize = HI_U64 as usize;
+
+        x.wrapping_sub(LO_USIZE) & !x & HI_USIZE != 0
+    }
+
+    #[inline(always)]
+    fn repeat_byte(b: u8) -> usize {
+        (b as usize) * (usize::MAX / 255)
+    }
+
+    #[inline(always)]
+    unsafe fn forward_search<F: Fn(u8) -> bool>(
+        start_ptr: *const u8,
+        mut ptr: *const u8,
+        confirm: F,
+    ) -> usize {
+        debug_assert!(start_ptr <= ptr);
+
+        loop {
+            if confirm(*ptr) {
+                return ptr as usize - start_ptr as usize;
+            }
+            ptr = ptr.offset(1);
+        }
+    }
+
+    unsafe fn ptr_add(ptr: *const u8, amt: usize) -> *const u8 {
+        debug_assert!(amt < ::core::isize::MAX as usize);
+        ptr.offset(amt as isize)
+    }
+
+    unsafe fn read_unaligned_usize(ptr: *const u8) -> usize {
+        let mut n: usize = 0;
+        core::ptr::copy_nonoverlapping(ptr, &mut n as *mut _ as *mut u8, USIZE_BYTES);
+        n
+    }
+
+    #[cfg(target_pointer_width = "32")]
+    const USIZE_BYTES: usize = 4;
+
+    #[cfg(target_pointer_width = "64")]
+    const USIZE_BYTES: usize = 8;
+
+    let vn1 = repeat_byte(n1);
+    let vn2 = repeat_byte(n2);
+    let confirm = |byte| byte == n1 || byte == n2;
+    let align = USIZE_BYTES - 1;
+    let start_ptr = haystack;
+    let mut ptr = start_ptr;
+
+    unsafe {
+        let chunk = read_unaligned_usize(ptr);
+        let eq1 = contains_zero_byte(chunk ^ vn1);
+        let eq2 = contains_zero_byte(chunk ^ vn2);
+        if eq1 || eq2 {
+            return forward_search(start_ptr, ptr, confirm);
+        }
+
+        ptr = ptr_add(ptr, USIZE_BYTES - (start_ptr as usize & align));
+        debug_assert!(ptr > start_ptr);
+        loop {
+            debug_assert_eq!(0, (ptr as usize) % USIZE_BYTES);
+
+            let chunk = *(ptr as *const usize);
+            let eq1 = contains_zero_byte(chunk ^ vn1);
+            let eq2 = contains_zero_byte(chunk ^ vn2);
+            if eq1 || eq2 {
+                break;
+            }
+            ptr = ptr_add(ptr, USIZE_BYTES);
+        }
+        forward_search(start_ptr, ptr, confirm)
+    }
+}
+
 #[no_mangle]
 pub unsafe extern "C" fn strchr(s: *const c_char, c: c_int) -> *mut c_char {
-    let haystack = slice::from_raw_parts(s as *const u8, usize::MAX);
-
-    match memchr::memchr2(c as u8, 0u8, haystack) {
-        Some(index) => match haystack[index] != 0 {
-            true => haystack[index..].as_ptr() as *mut c_char,
-            false => ptr::null_mut(),
-        },
-        None => ptr::null_mut(),
+    let index = rawmemchr2(c as u8, 0u8, s as *const u8);
+    if *s.offset(index) != 0 {
+        s.offset(index) as *mut c_char
+    }
+    else {
+        ptr::null_mut()
     }
 }
 
@@ -263,7 +425,7 @@ pub unsafe extern "C" fn strerror_r(errnum: c_int, buf: *mut c_char, buflen: siz
 
 #[no_mangle]
 pub unsafe extern "C" fn strlen(s: *const c_char) -> size_t {
-    strnlen(s, usize::MAX)
+    rawmemchr2(0u8, 0u8, s as *const u8) as size_t
 }
 
 #[no_mangle]
