2bc667f71c11b75c4f6bab82e9a5fdb529ba708c
Tiago Lam <tiagolam@gmail.com>
2020-02-05 22:32:12 +0000

    header/sys_un: Set sockaddr_un members to public.
    Future commits will make use of this, in order to support AF_UNIX
    sockets.

--- src/header/sys_un/mod.rs
+++ src/header/sys_un/mod.rs
@@ -2,6 +2,6 @@ use crate::{header::sys_socket::sa_family_t, platform::types::*};
 
 #[repr(C)]
 pub struct sockaddr_un {
-    sun_family: sa_family_t,
-    sun_path: [c_char; 108],
+    pub sun_family: sa_family_t,
+    pub sun_path: [c_char; 108],
 }
