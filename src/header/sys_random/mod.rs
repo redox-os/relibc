0cc0fbecdc40956a74863154d4eb9109b97db3d2
Jeremy Soller <jeremy@system76.com>
2020-05-22 15:49:51 -0600

    Export getrandom function

--- src/header/sys_random/mod.rs
+++ src/header/sys_random/mod.rs
@@ -6,7 +6,7 @@ pub const GRND_NONBLOCK: c_uint = 1;
 pub const GRND_RANDOM: c_uint = 2;
 
 #[no_mangle]
-unsafe extern "C" fn getrandom(buf: *mut c_void, buflen: size_t, flags: c_uint) -> ssize_t {
+pub unsafe extern "C" fn getrandom(buf: *mut c_void, buflen: size_t, flags: c_uint) -> ssize_t {
     Sys::getrandom(slice::from_raw_parts_mut(
         buf as *mut u8,
         buflen as usize
