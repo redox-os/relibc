e4a7186b22949db41b55ae8c3bc5e473d52accdc
Peter Limkilde Svendsen <peter.limkilde@gmail.com>
2020-05-28 20:17:57 +0200

    Use C type names in byteorder functions

--- src/header/arpa_inet/mod.rs
+++ src/header/arpa_inet/mod.rs
@@ -16,22 +16,22 @@ use crate::{
 };
 
 #[no_mangle]
-pub extern "C" fn htonl(hostlong: u32) -> u32 {
+pub extern "C" fn htonl(hostlong: uint32_t) -> uint32_t {
     hostlong.to_be()
 }
 
 #[no_mangle]
-pub extern "C" fn htons(hostshort: u16) -> u16 {
+pub extern "C" fn htons(hostshort: uint16_t) -> uint16_t {
     hostshort.to_be()
 }
 
 #[no_mangle]
-pub extern "C" fn ntohl(netlong: u32) -> u32 {
+pub extern "C" fn ntohl(netlong: uint32_t) -> uint32_t {
     u32::from_be(netlong)
 }
 
 #[no_mangle]
-pub extern "C" fn ntohs(netshort: u16) -> u16 {
+pub extern "C" fn ntohs(netshort: uint16_t) -> uint16_t {
     u16::from_be(netshort)
 }
 
