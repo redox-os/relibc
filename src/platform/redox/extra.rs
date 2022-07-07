1476cdeb8f6a497a5a6659eba6763142ebe9e099
4lDO2 <4lDO2@protonmail.com>
2022-07-07 19:27:11 +0200

    Split fexec_impl into crate, used by escalated.

--- src/platform/redox/extra.rs
+++ src/platform/redox/extra.rs
@@ -48,41 +48,4 @@ pub unsafe extern "C" fn redox_physunmap(virtual_address: *mut c_void) -> c_int 
     e(syscall::physunmap(virtual_address as usize)) as c_int
 }
 
-pub struct FdGuard {
-    fd: usize,
-    taken: bool,
-}
-impl FdGuard {
-    pub fn new(fd: usize) -> Self {
-        Self {
-            fd, taken: false,
-        }
-    }
-    pub fn take(&mut self) -> usize {
-        self.taken = true;
-        self.fd
-    }
-}
-impl core::ops::Deref for FdGuard {
-    type Target = usize;
-
-    fn deref(&self) -> &Self::Target {
-        &self.fd
-    }
-}
-
-impl Drop for FdGuard {
-    fn drop(&mut self) {
-        if !self.taken {
-            let _ = syscall::close(self.fd);
-        }
-    }
-}
-pub fn create_set_addr_space_buf(space: usize, ip: usize, sp: usize) -> [u8; size_of::<usize>() * 3] {
-    let mut buf = [0_u8; 3 * size_of::<usize>()];
-    let mut chunks = buf.array_chunks_mut::<{size_of::<usize>()}>();
-    *chunks.next().unwrap() = usize::to_ne_bytes(space);
-    *chunks.next().unwrap() = usize::to_ne_bytes(sp);
-    *chunks.next().unwrap() = usize::to_ne_bytes(ip);
-    buf
-}
+pub use redox_exec::{create_set_addr_space_buf, FdGuard};
