334e33cfde9e2c152d8ee2a30d2d9126aee0de01
Jeremy Soller <jackpot51@gmail.com>
2023-03-10 10:45:19 -0700

    Fix abort definition for C++

--- include/bits/stdlib.h
+++ include/bits/stdlib.h
@@ -2,15 +2,15 @@
 #define _BITS_STDLIB_H
 
 #ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
 // C++ needs abort to be a function, define backup function
 void abort(void);
 #else
 // C uses detailed abort macro
 #define abort() __abort(__func__, __FILE__, __LINE__)
-#endif
-
-#ifdef __cplusplus
-extern "C" {
 #endif
 
 long double strtold(const char *nptr, char **endptr);
--- src/c/stdlib.c
+++ src/c/stdlib.c
@@ -8,7 +8,7 @@ long double strtold(const char *nptr, char **endptr) {
 void __abort(const char *func, const char *file, int line);
 
 // backup definition of abort for programs that link it directly
-void abort() {
+void abort(void) {
     // call detailed abort function
     __abort(__func__, __FILE__, __LINE__);
 }
