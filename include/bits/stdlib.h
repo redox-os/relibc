0646140d99d14183456ee0b7dcd4d61aaa975fdc
Jeremy Soller <jackpot51@gmail.com>
2023-03-10 11:33:53 -0700

    Remove detailed abort, too many compatibility issues

--- include/bits/stdlib.h
+++ include/bits/stdlib.h
@@ -1,12 +1,6 @@
 #ifndef _BITS_STDLIB_H
 #define _BITS_STDLIB_H
 
-// C++ needs abort to be a function, and cannot use this
-#ifndef __cplusplus
-// Override abort function with detailed abort in C
-#define abort() __abort(__func__, __FILE__, __LINE__)
-#endif
-
 #ifdef __cplusplus
 extern "C" {
 #endif
--- src/c/stack_chk.c
+++ src/c/stack_chk.c
@@ -2,11 +2,9 @@
 
 uintptr_t __stack_chk_guard = 0xd048c37519fcadfe;
 
-// manually define detailed abort function
-void __abort(const char *func, const char *file, int line) __attribute__((noreturn));
+void abort(void) __attribute__((noreturn));
 
 __attribute__((noreturn))
 void __stack_chk_fail(void) {
-    // call detailed abort function
-    __abort(__func__, __FILE__, __LINE__);
+    abort();
 }
