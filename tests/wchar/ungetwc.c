c659bf11f9f23fd65159c71949677bcb31018616
Darley Barreto <darleybarreto@gmail.com>
2023-05-09 14:23:25 +0000

    Implementing ungetwc

---
+++ tests/wchar/ungetwc.c
@@ -0,0 +1,26 @@
+#include <wchar.h>
+#include <wctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+ 
+int main()
+{
+   FILE *stream;
+   wint_t wc;
+   wint_t wc2;
+
+   if (NULL == (stream = fopen("wchar/ungetwc.in", "r+")))
+      return 1;
+ 
+   while (WEOF != (wc = fgetwc(stream)) && iswdigit(wc)) {}
+ 
+   if (WEOF != wc)
+      ungetwc(wc, stream);
+   
+   wc2 = fgetwc(stream);
+   assert(WEOF != wc2);
+   assert(wc == wc2);
+   
+   return 0;
+}
