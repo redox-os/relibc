14709b3d5c63bc45a8f0bce53133f65c5543352d
Florian Meißner <florian.meissner@mailbox.org>
2023-05-29 20:54:34 +0200

    system(): on command == NULL, return nonzero if shell exists

--- tests/stdlib/system.c
+++ tests/stdlib/system.c
@@ -3,6 +3,16 @@
 #include "test_helpers.h"
 
 int main(void) {
-    int status = system("echo test of system");
+    // testing shell detection
+    // this means, because we don't detect if a shell actually exists but just assume it does, this test case breaks in
+    // environments without `sh`. I think that is a reasonable tradeoff.
+    // (And if there isn't a shell, system() won't work anyways)
+    int status = system(NULL);
+    printf("shell found: %i\n", status);
+    fflush(stdout);
+    ERROR_IF(system, status, == 0);
+
+    // base case
+    status = system("echo test of system");
     ERROR_IF(system, status, == -1);
 }
