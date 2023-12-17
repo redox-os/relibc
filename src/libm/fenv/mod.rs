a974c9fbac0bd9d1f61fb374140e8a0957142e63
Darley Barreto <darleybarreto@gmail.com>
2023-12-17 17:38:47 -0300

    Add constants to fenv

--- src/libm/fenv/mod.rs
+++ src/libm/fenv/mod.rs
@@ -3,15 +3,63 @@
 
 use crate::platform::types::*;
 
-pub const FE_ALL_EXCEPT: c_int = 0;
-pub const FE_TONEAREST: c_int = 0;
+#[cfg(target_arch = "x86_64")]
+mod  consts {
+    use super::c_int;
 
-pub type fexcept_t = u64;
+    pub const  FE_INVALID: c_int = 1;
+    pub const  FE_DIVBYZERO: c_int = 4;
+    pub const  FE_OVERFLOW: c_int = 8;
+    pub const  FE_UNDERFLOW: c_int = 16;
+    pub const  FE_INEXACT: c_int = 32;
 
-#[repr(C)]
-pub struct fenv_t {
-    pub cw: u64,
+    pub const  FE_ALL_EXCEPT: c_int = 63;
+
+    pub const  FE_TONEAREST: c_int = 0;
+    pub const  FE_DOWNWARD: c_int = 0x400;
+    pub const  FE_UPWARD: c_int = 0x800;
+    pub const  FE_TOWARDZERO: c_int = 0xc00;
 }
+
+#[cfg(target_arch = "aarch64")]
+mod  consts {
+    use super::c_int;
+
+    pub const  FE_INVALID: c_int = 1;
+    pub const  FE_DIVBYZERO: c_int = 2;
+    pub const  FE_OVERFLOW: c_int = 4;
+    pub const  FE_UNDERFLOW: c_int = 8;
+    pub const  FE_INEXACT: c_int = 16;
+    pub const  FE_ALL_EXCEPT: c_int = 31;
+
+    pub const  FE_TONEAREST: c_int = 0;
+    pub const  FE_DOWNWARD: c_int = 0x800000;
+    pub const  FE_UPWARD: c_int = 0x400000;
+    pub const  FE_TOWARDZERO: c_int = 0xc00000;
+}
+
+#[cfg(target_arch = "riscv64")]
+mod  consts {
+    use super::c_int;
+
+    pub const  FE_INVALID: c_int = 16;
+    pub const  FE_DIVBYZERO: c_int = 8;
+    pub const  FE_OVERFLOW: c_int = 4;
+    pub const  FE_UNDERFLOW: c_int = 2;
+    pub const  FE_INEXACT: c_int = 1;
+
+    pub const  FE_ALL_EXCEPT: c_int = 31;
+
+    pub const  FE_TONEAREST: c_int = 0;
+    pub const  FE_DOWNWARD: c_int = 2;
+    pub const  FE_UPWARD: c_int = 3;
+    pub const  FE_TOWARDZERO: c_int = 1;
+}
+
+pub use consts::*;
+
+pub type fexcept_t = u16;
+pub type fenv_t = u64;
 
 // #[no_mangle]
 pub unsafe extern "C" fn feclearexcept(excepts: c_int) -> c_int {
