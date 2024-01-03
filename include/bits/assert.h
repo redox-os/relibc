2a40638ec362145be2e2873e1d0fde5fe3ad0b6e
Jeremy Soller <jackpot51@gmail.com>
2024-01-03 13:02:51 -0700

    Fix assert definition

--- include/bits/assert.h
+++ include/bits/assert.h
@@ -1,11 +1,11 @@
-#ifndef _BITS_ASSERT_H
-#define _BITS_ASSERT_H
+// Do not use include guard, to ensure assert is always defined
+#ifdef assert
+#undef assert
+#endif
 
 #ifdef NDEBUG
 # define assert(cond) (void) 0
 #else
 # define assert(cond) \
   ((void)((cond) || (__assert_fail(__func__, __FILE__, __LINE__, #cond), 0)))
-#endif
-
 #endif
