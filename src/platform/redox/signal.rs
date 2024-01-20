928ec053d9c5044011b818b198b565122c98a38c
4lDO2 <4lDO2@protonmail.com>
2024-01-20 17:06:17 +0100

    Probably make signals work.

--- src/platform/redox/signal.rs
+++ src/platform/redox/signal.rs
@@ -1,5 +1,5 @@
 use core::mem;
-use syscall::{self, Result, number::SYS_SIGRETURN};
+use syscall::{self, Result, number::SYS_SIGRETURN, SignalStack};
 
 use super::{
     super::{types::*, Pal, PalSignal},
@@ -16,7 +16,7 @@ use crate::{
 };
 
 impl PalSignal for Sys {
-    fn getitimer(which: c_int, out: *mut itimerval) -> c_int {
+    unsafe fn getitimer(which: c_int, out: *mut itimerval) -> c_int {
         let path = match which {
             ITIMER_REAL => "itimer:1",
             _ => {
@@ -39,12 +39,10 @@ impl PalSignal for Sys {
             return -1;
         }
 
-        unsafe {
-            (*out).it_interval.tv_sec = spec.it_interval.tv_sec as time_t;
-            (*out).it_interval.tv_usec = spec.it_interval.tv_nsec / 1000;
-            (*out).it_value.tv_sec = spec.it_value.tv_sec as time_t;
-            (*out).it_value.tv_usec = spec.it_value.tv_nsec / 1000;
-        }
+        (*out).it_interval.tv_sec = spec.it_interval.tv_sec as time_t;
+        (*out).it_interval.tv_usec = spec.it_interval.tv_nsec / 1000;
+        (*out).it_value.tv_sec = spec.it_value.tv_sec as time_t;
+        (*out).it_value.tv_usec = spec.it_value.tv_nsec / 1000;
 
         0
     }
@@ -61,7 +59,7 @@ impl PalSignal for Sys {
         Self::kill(Self::getpid(), sig)
     }
 
-    fn setitimer(which: c_int, new: *const itimerval, old: *mut itimerval) -> c_int {
+    unsafe fn setitimer(which: c_int, new: *const itimerval, old: *mut itimerval) -> c_int {
         let path = match which {
             ITIMER_REAL => "itimer:1",
             _ => {
@@ -110,25 +108,25 @@ impl PalSignal for Sys {
         e(sigaction_impl(sig, act, oact).map(|()| 0)) as c_int
     }
 
-    fn sigaltstack(ss: *const stack_t, old_ss: *mut stack_t) -> c_int {
+    unsafe fn sigaltstack(ss: *const stack_t, old_ss: *mut stack_t) -> c_int {
         unimplemented!()
     }
 
-    fn sigpending(set: *mut sigset_t) -> c_int {
+    unsafe fn sigpending(set: *mut sigset_t) -> c_int {
         ERRNO.set(ENOSYS);
         -1
     }
 
-    fn sigprocmask(how: c_int, set: *const sigset_t, oset: *mut sigset_t) -> c_int {
+    unsafe fn sigprocmask(how: c_int, set: *const sigset_t, oset: *mut sigset_t) -> c_int {
         e(sigprocmask_impl(how, set, oset).map(|()| 0)) as c_int
     }
 
-    fn sigsuspend(set: *const sigset_t) -> c_int {
+    unsafe fn sigsuspend(set: *const sigset_t) -> c_int {
         ERRNO.set(ENOSYS);
         -1
     }
 
-    fn sigtimedwait(set: *const sigset_t, sig: *mut siginfo_t, tp: *const timespec) -> c_int {
+    unsafe fn sigtimedwait(set: *const sigset_t, sig: *mut siginfo_t, tp: *const timespec) -> c_int {
         ERRNO.set(ENOSYS);
         -1
     }
@@ -140,11 +138,10 @@ pub(crate) fn sigaction_impl(
     oact: Option<&mut sigaction>,
 ) -> Result<()> {
     let new_opt = act.map(|act| {
-        let m = act.sa_mask;
         let sa_handler = unsafe { mem::transmute(act.sa_handler) };
         syscall::SigAction {
             sa_handler,
-            sa_mask: [m as u64, 0],
+            sa_mask: act.sa_mask as u64,
             sa_flags: syscall::SigActionFlags::from_bits(act.sa_flags as usize)
                 .expect("sigaction: invalid bit pattern"),
         }
@@ -153,23 +150,13 @@ pub(crate) fn sigaction_impl(
     syscall::sigaction(sig as usize, new_opt.as_ref(), old_opt.as_mut())?;
     if let (Some(old), Some(oact)) = (old_opt, oact) {
         oact.sa_handler = unsafe { mem::transmute(old.sa_handler) };
-        let m = old.sa_mask;
-        oact.sa_mask = m[0] as sigset_t;
+        oact.sa_mask = old.sa_mask as sigset_t;
         oact.sa_flags = old.sa_flags.bits() as c_ulong;
     }
     Ok(())
 }
-pub(crate) fn sigprocmask_impl(how: i32, set: *const sigset_t, oset: *mut sigset_t) -> Result<()> {
-    let new_opt = if set.is_null() {
-        None
-    } else {
-        Some([unsafe { *set as u64 }, 0])
-    };
-    let mut old_opt = if oset.is_null() { None } else { Some([0, 0]) };
-    syscall::sigprocmask(how as usize, new_opt.as_ref(), old_opt.as_mut())?;
-    if let Some(old) = old_opt {
-        unsafe { *oset = old[0] as sigset_t };
-    }
+pub(crate) unsafe fn sigprocmask_impl(how: i32, set: *const sigset_t, oset: *mut sigset_t) -> Result<()> {
+    syscall::sigprocmask(how as usize, set.as_ref(), oset.as_mut())?;
     Ok(())
 }
 #[cfg(target_arch = "x86_64")]
@@ -217,15 +204,37 @@ pub fn setup_sighandler() {
     let _ = syscall::close(fd);
 }
 
-unsafe extern "C" fn inner(stack: usize) {
+#[repr(C)]
+pub struct SigStack {
+    #[cfg(target_arch = "x86_64")]
+    fx: [u8; 4096],
+
+    #[cfg(target_arch = "x86")]
+    fx: [u8; 512],
+
+    kernel_pushed: SignalStack,
 }
 
+#[inline(always)]
+unsafe fn inner(stack: &mut SigStack) {
+    let handler: extern "C" fn(c_int) = core::mem::transmute(stack.kernel_pushed.sa_handler);
+    handler(stack.kernel_pushed.sig_num as c_int)
+}
+#[cfg(not(target_arch = "x86"))]
+unsafe extern "C" fn inner_c(stack: usize) {
+    inner(&mut *(stack as *mut SigStack))
+}
+#[cfg(target_arch = "x86")]
+unsafe extern "fastcall" fn inner_fastcall(stack: usize) {
+    inner(&mut *(stack as *mut SigStack))
+}
+
+// TODO: is the memset necessary?
 #[cfg(target_arch = "x86_64")]
 asmfunction!(__relibc_internal_sigentry_xsave: ["
-    and rsp, -64
+    sub rsp, 4096
     mov eax, 0xffffffff
     mov edx, eax
-    sub rsp, 4096
 
     mov rdi, rsp
     xor eax, eax
@@ -244,10 +253,11 @@ asmfunction!(__relibc_internal_sigentry_xsave: ["
 
     mov eax, {SYS_SIGRETURN}
     syscall
-"] <= [inner = sym inner, SYS_SIGRETURN = const SYS_SIGRETURN]);
+"] <= [inner = sym inner_c, SYS_SIGRETURN = const SYS_SIGRETURN]);
+
 #[cfg(target_arch = "x86_64")]
 asmfunction!(__relibc_internal_sigentry_fxsave: ["
-    sub rsp, 512
+    sub rsp, 4096
 
     mov rdi, rsp
     xor eax, eax
@@ -260,8 +270,17 @@ asmfunction!(__relibc_internal_sigentry_fxsave: ["
     call {inner}
 
     fxrstor64 [rsp]
-    add rsp, 512
+    add rsp, 4096
 
     mov eax, {SYS_SIGRETURN}
     syscall
-"] <= [inner = sym inner, SYS_SIGRETURN = const SYS_SIGRETURN]);
+"] <= [inner = sym inner_c, SYS_SIGRETURN = const SYS_SIGRETURN]);
+
+#[cfg(target_arch = "x86")]
+asmfunction!(__relibc_internal_sigentry, ["
+    sub esp, 512
+    fxsave [esp]
+
+    mov ecx, esp
+    call {inner}
+"] <= [inner = sym inner_fastcall, SYS_SIGRETURN = const SYS_SIGRETURN]);
