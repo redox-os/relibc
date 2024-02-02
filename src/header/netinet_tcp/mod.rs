47ac9e2d0baa3783aca01b6c3e02386b3154a83f
Jeremy Soller <jackpot51@gmail.com>
2024-02-02 15:31:08 -0700

    Add more TCP socket options

--- src/header/netinet_tcp/mod.rs
+++ src/header/netinet_tcp/mod.rs
@@ -1,3 +1,7 @@
 use crate::platform::types::*;
 
 pub const TCP_NODELAY: c_int = 1;
+pub const TCP_MAXSEG: c_int = 2;
+pub const TCP_KEEPIDLE: c_int = 4;
+pub const TCP_KEEPINTVL: c_int = 5;
+pub const TCP_KEEPCNT: c_int = 6;
