70149c2db0fd1f11a6c5ed97da1442bdcac07dd0
Nicolás Antinori <nicolas_antinori@hotmail.com>
2024-02-18 01:56:41 -0300

    swscanf working as common scanf

--- tests/wchar/swscanf.c
+++ tests/wchar/swscanf.c
@@ -6,14 +6,17 @@
 
 int main ()
 {
-  wchar_t sentence [] = L"10";
-  // wchar_t str [20];
+  wchar_t sentence [] = L"Michael 10";
+  wchar_t str [20];
   int i;
 
-  printf("Jelou");
-  wint_t status = swscanf (sentence,L"%d",&i);
-  printf("%d", status);
-  wprintf (L"%d\n",i);
+  printf("Jelou\n");
+  // wint_t status = swscanf (sentence,L"%*s %d",str,&i);
+  wint_t status = swscanf (sentence,L"%s %d",str,&i);
+  printf("%d\n", status);
+  printf("%d\n", i);
+  printf("%s\n", str);
+  wprintf (L"%ls -> %d\n",str,i);
   ERROR_IF(swscanf, status, == WEOF);
 
   return 0;
