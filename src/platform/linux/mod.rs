bff12748e2e4cb2e58e1105998cc474a5a62f7fc
4lDO2 <4lDO2@protonmail.com>
2024-08-05 13:22:18 +0200

    Properly deallocate thread stack on pthread_exit.

--- src/platform/linux/mod.rs
+++ src/platform/linux/mod.rs
@@ -155,7 +155,7 @@ impl Pal for Sys {
         }
         loop {}
     }
-    fn exit_thread() -> ! {
+    unsafe fn exit_thread(_stack_base: *mut (), _stack_size: usize) -> ! {
         // TODO
         Self::exit(0)
     }
