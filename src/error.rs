b9653e689bd1f4d94e670315c7a4c4a0cac1785e
4lDO2 <4lDO2@protonmail.com>
2024-09-07 12:48:38 +0200

    Implement From<Errno> for io::Error.

--- src/error.rs
+++ src/error.rs
@@ -7,14 +7,23 @@ pub struct Errno(pub c_int);
 
 #[cfg(target_os = "redox")]
 impl From<syscall::Error> for Errno {
+    #[inline]
     fn from(value: syscall::Error) -> Self {
         Errno(value.errno)
     }
 }
 #[cfg(target_os = "redox")]
 impl From<Errno> for syscall::Error {
+    #[inline]
     fn from(value: Errno) -> Self {
         syscall::Error::new(value.0)
+    }
+}
+
+impl From<Errno> for crate::io::Error {
+    #[inline]
+    fn from(Errno(errno): Errno) -> Self {
+        Self::from_raw_os_error(errno)
     }
 }
 
@@ -25,10 +34,10 @@ impl<T: From<i8>> ResultExt<T> for Result<T, Errno> {
     fn or_minus_one_errno(self) -> T {
         match self {
             Self::Ok(v) => v,
-            Self::Err(Errno(errno)) => unsafe {
+            Self::Err(Errno(errno)) => {
                 crate::platform::ERRNO.set(errno);
                 T::from(-1)
-            },
+            }
         }
     }
 }
