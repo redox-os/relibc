41264dc8b145a844e3ab3977d837b8a63b3f36f1
Peter Limkilde Svendsen <peter.limkilde@gmail.com>
2024-10-22 22:12:24 +0000

    Add NulTerminatedInclusive iterator, rewrite/fix strchr/wcschr

--- tests/string/strchr.c
+++ tests/string/strchr.c
@@ -6,5 +6,6 @@
 int main(void) {
 	printf("%s\n", strchr("hello", 'e')); // should be ello
 	printf("%s\n", strchr("world", 'l')); // should be ld
-	printf("%i\n", strchr("world", 0) == NULL); // should be 1
+	printf("%s\n", strchr("world", '\0')); // should be an empty, nul-terminated string
+	printf("%p\n", strchr("world", 'x')); // should be a null pointer
 }
