913e7f7849c0cb5afa977d613dacae51aa55364a
Peter Limkilde Svendsen <peter.limkilde@gmail.com>
2024-12-18 22:13:00 +0100

    Also use MaybeUninit for source slice

--- src/header/string/mod.rs
+++ src/header/string/mod.rs
@@ -90,7 +90,7 @@ pub unsafe extern "C" fn memcmp(s1: *const c_void, s2: *const c_void, n: size_t)
 /// - `n` is 0, *or*
 ///     - `s1` is convertible to a `&mut [MaybeUninit<u8>]` with length `n`,
 ///       and
-///     - `s2` is convertible to a `&[u8]` with length `n`.
+///     - `s2` is convertible to a `&[MaybeUninit<u8>]` with length `n`.
 #[no_mangle]
 pub unsafe extern "C" fn memcpy(s1: *mut c_void, s2: *const c_void, n: size_t) -> *mut c_void {
     // Avoid creating slices for n == 0. This is because we are required to
@@ -103,15 +103,15 @@ pub unsafe extern "C" fn memcpy(s1: *mut c_void, s2: *const c_void, n: size_t) -
         // isize::MAX; this implicitly ensured by requiring valid pointers to
         // two nonoverlapping slices.
         let s1_slice = unsafe { slice::from_raw_parts_mut(s1.cast::<MaybeUninit<u8>>(), n) };
-        let s2_slice = unsafe { slice::from_raw_parts(s2.cast::<u8>(), n) };
+        let s2_slice = unsafe { slice::from_raw_parts(s2.cast::<MaybeUninit<u8>>(), n) };
 
-        // It may seem tempting to use MaybeUninit::copy_from_slice() here, but
-        // memcpy is one of the handful of symbols whose existence is assumed
-        // by Rust's core library, and thus we need to be careful here not to
-        // rely on any function that calls memcpy internally.
+        // It may seem tempting to use s1.copy_from_slice(s2) here, but memcpy
+        // is one of the handful of symbols whose existence is assumed by
+        // Rust's core library, and thus we need to be careful here not to rely
+        // on any function that calls memcpy internally.
         // See https://doc.rust-lang.org/core/index.html for details.
         for (s1_elem, s2_elem) in zip(s1_slice, s2_slice) {
-            s1_elem.write(*s2_elem);
+            *s1_elem = *s2_elem;
         }
     }
     s1
