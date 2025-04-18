4df4381a90e38174b37a39328c5d7c291cf94df2
Josh Megnauth <jo.sh@tutanota.com>
2025-04-18 01:39:16 -0400

    Const calculate buffer size for strerror
    The current buffer size is hard set to 256. Using const evaluation, the
    maximum buf size can be calculated at compile time.

--- tests/error.c
+++ tests/error.c
@@ -1,7 +1,9 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
+#include <assert.h>
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "test_helpers.h"
 
@@ -12,15 +14,23 @@ int main(void) {
     printf("errno: %d = %s\n", err, strerror(errno));
     perror("perror");
 
-    char buf1[256];
+    char buf1[256] = {0};
     int ret1 = strerror_r(err, buf1, 256);
     printf("errno: %d = %s, return: %d\n", err, buf1, ret1);
 
-    char buf2[3];
+    char buf2[3] = {0};
     int ret2 = strerror_r(err, buf2, 3);
     printf("errno: %d = %s, return: %d\n", err, buf2, ret2);
 
     char buf3[256] = {0};
     int ret3 = strerror_r(err, buf3, 0);
     printf("errno: %d = %s, return: %d\n", err, buf3, ret3);
+
+    // Test that the statically allocated buffer doesn't overflow
+    for (int code = EPERM; code < ENOTRECOVERABLE; ++code) {
+        const char* message = strerror(code);
+        assert(message);
+    }
+
+    return EXIT_SUCCESS;
 }
