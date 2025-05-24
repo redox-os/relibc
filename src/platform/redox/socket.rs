7a944d42bf98f8c06764b9a94b5097f38aa0a633
Ibuki.O <isan.ethen@gmail.com>
2025-05-24 15:10:19 +0900

    feat: Implement senmsg and recvmsg for uds.

--- src/platform/redox/socket.rs
+++ src/platform/redox/socket.rs
@@ -15,7 +15,8 @@ use crate::{
         netinet_in::{in_addr, in_port_t, sockaddr_in},
         string::strnlen,
         sys_socket::{
-            constants::*, msghdr, sa_family_t, sockaddr, socklen_t, CMSG_FIRSTHDR, CMSG_NXTHDR,
+            constants::*, msghdr, sa_family_t, sockaddr, socklen_t, ucred, CMSG_ALIGN, CMSG_DATA,
+            CMSG_FIRSTHDR, CMSG_NXTHDR,
         },
         sys_time::timeval,
         sys_un::sockaddr_un,
@@ -182,6 +183,10 @@ unsafe fn inner_get_name(
         inner_af_unix(&buf[5..], address, address_len);
     } else if buf.starts_with(b"/scheme/chan/") {
         inner_af_unix(&buf[13..], address, address_len);
+    } else if buf.starts_with(b"/scheme/uds_stream/") {
+        inner_af_unix(&buf[19..], address, address_len);
+    } else if buf.starts_with(b"/scheme/uds_dgram/") {
+        inner_af_unix(&buf[18..], address, address_len);
     } else {
         // Socket doesn't belong to any scheme
         panic!(
@@ -311,29 +316,254 @@ impl PalSocket for Sys {
     }
 
     unsafe fn recvmsg(socket: c_int, msg: *mut msghdr, flags: c_int) -> Result<usize> {
-        //TODO: implement recvfrom with recvmsg
-        eprintln!(
-            "recvmsg({}, {:p}, {:#x}) not implemented on redox",
-            socket, msg, flags
-        );
-        if !msg.is_null() {
-            let msg = &*msg;
-            eprintln!("{:#x?}", msg);
-            if !msg.msg_control.is_null() {
-                let mut cmsg = CMSG_FIRSTHDR(msg);
-                while !cmsg.is_null() {
-                    eprintln!("{:#x?}", *cmsg);
-                    cmsg = CMSG_NXTHDR(msg, cmsg);
+        if msg.is_null() {
+            return Err(Errno(EINVAL));
+        }
+        let mhdr = &mut *msg;
+
+        // 1. Read the size of the ancillary data.
+        let mut ancillary_len_bytes = [0u8; mem::size_of::<usize>()];
+        Self::read(socket, &mut ancillary_len_bytes)?;
+        let ancillary_len = usize::from_le_bytes(ancillary_len_bytes);
+
+        enum AncillaryType {
+            Rights(Vec<c_int>),
+            Credentials(ucred),
+        }
+        let mut ancillary_data: VecDeque<AncillaryType> = VecDeque::new();
+
+        // 2. Read and parse the serialized ancillary data.
+        if ancillary_len > 0 {
+            let mut ancillary_data_buffer = vec![0u8; ancillary_len];
+            Self::read(socket, &mut ancillary_data_buffer)?;
+
+            let mut cursor = 0;
+            while cursor < ancillary_len {
+                let level_bytes = ancillary_data_buffer[cursor..cursor + 4]
+                    .try_into()
+                    .unwrap();
+                let type_bytes = ancillary_data_buffer[cursor + 4..cursor + 8]
+                    .try_into()
+                    .unwrap();
+                let cmsg_level = u32::from_le_bytes(level_bytes) as c_int;
+                let cmsg_type = u32::from_le_bytes(type_bytes) as c_int;
+                cursor += 8;
+
+                match (cmsg_level, cmsg_type) {
+                    (SOL_SOCKET, SCM_RIGHTS) => {
+                        let mut received_fds: Vec<c_int> = Vec::new();
+                        let count_bytes = ancillary_data_buffer[cursor..cursor + 4]
+                            .try_into()
+                            .unwrap();
+                        let fd_count = u32::from_le_bytes(count_bytes);
+                        cursor += 4;
+
+                        for _ in 0..fd_count {
+                            let new_fd = syscall::dup(socket as usize, b"recvfd")?;
+                            received_fds.push(new_fd as c_int);
+                        }
+                        ancillary_data.push_back(AncillaryType::Rights(received_fds));
+                    }
+                    (SOL_SOCKET, SCM_CREDENTIALS) => {
+                        let cred_id = FdGuard::new(syscall::dup(socket as usize, b"cred")?);
+
+                        // Assuming the read returns pid, uid, gid packed together.
+                        let mut cred_buf =
+                            vec![0u8; mem::size_of::<usize>() + mem::size_of::<u32>() * 2];
+                        Self::read(*cred_id as c_int, &mut cred_buf)?;
+                        Self::close(*cred_id)?;
+
+                        let pid = usize::from_le_bytes(
+                            cred_buf[0..mem::size_of::<usize>()].try_into().unwrap(),
+                        );
+                        let uid = u32::from_le_bytes(
+                            cred_buf[mem::size_of::<usize>()..mem::size_of::<usize>() + 4]
+                                .try_into()
+                                .unwrap(),
+                        );
+                        let gid = u32::from_le_bytes(
+                            cred_buf[mem::size_of::<usize>() + 4..].try_into().unwrap(),
+                        );
+
+                        ancillary_data.push_back(AncillaryType::Credentials(ucred {
+                            pid,
+                            uid,
+                            gid,
+                        }));
+                    }
+                    _ => {
+                        // Unknown type, skip.
+                        return Err(Errno(EINVAL));
+                    }
                 }
             }
         }
-        Err(Errno(ENOSYS))
+
+        // 3. Reconstruct the ancillary data in the user-provided buffer.
+        mhdr.msg_controllen = 0;
+        if !mhdr.msg_control.is_null() && mhdr.msg_controllen > 0 {
+            let mut cmsg_space = mhdr.msg_controllen;
+            let cmsg_ptr = mhdr.msg_control as *mut cmsghdr;
+            let mut current_cmsg = CMSG_FIRSTHDR(mhdr);
+            let mut written_len = 0;
+
+            while let Some(ancillary_data) = ancillary_data.pop_front() {
+                match ancillary_data {
+                    AncillaryType::Rights(received_fds) => {
+                        // Reconstruct SCM_RIGHTS
+                        let data_len = mem::size_of::<c_int>() * received_fds.len();
+                        let space_needed = CMSG_SPACE(data_len as u32) as usize;
+
+                        if !current_cmsg.is_null() && cmsg_space >= space_needed {
+                            let cmsg = &mut *current_cmsg;
+                            cmsg.cmsg_len = CMSG_LEN(data_len as u32) as usize;
+                            cmsg.cmsg_level = SOL_SOCKET;
+                            cmsg.cmsg_type = SCM_RIGHTS;
+
+                            let data_ptr = CMSG_DATA(cmsg);
+                            std::ptr::copy_nonoverlapping(
+                                received_fds.as_ptr(),
+                                data_ptr as *mut c_int,
+                                received_fds.len(),
+                            );
+
+                            written_len += CMSG_ALIGN(cmsg.cmsg_len);
+                            cmsg_space -= CMSG_ALIGN(cmsg.cmsg_len);
+                            current_cmsg = CMSG_NXTHDR(mhdr, current_cmsg);
+                        }
+                    }
+                    AncillaryType::Credentials(cred) => {
+                        // Reconstruct SCM_CREDENTIALS
+                        let data_len = mem::size_of::<ucred>();
+                        let space_needed = CMSG_SPACE(data_len as u32) as usize;
+
+                        if !current_cmsg.is_null() && cmsg_space >= space_needed {
+                            let cmsg = &mut *current_cmsg;
+                            cmsg.cmsg_len = CMSG_LEN(data_len as u32) as usize;
+                            cmsg.cmsg_level = SOL_SOCKET;
+                            cmsg.cmsg_type = SCM_CREDENTIALS;
+
+                            let data_ptr = CMSG_DATA(cmsg);
+                            *(data_ptr as *mut ucred) = cred;
+
+                            written_len += CMSG_ALIGN(cmsg.cmsg_len);
+                        }
+                    }
+                }
+            }
+            mhdr.msg_controllen = written_len;
+        }
+
+        // 4. Read the message body into iov.
+        let mut bytes_read_to_iov = 0;
+        if mhdr.msg_iovlen > 0 {
+            let iovs = slice::from_raw_parts_mut(mhdr.msg_iov, mhdr.msg_iovlen as usize);
+            for iov in iovs {
+                if !iov.iov_base.is_null() && iov.iov_len > 0 {
+                    let data_slice =
+                        slice::from_raw_parts_mut(iov.iov_base as *mut u8, iov.iov_len);
+                    let read = Self::read(socket, data_slice)?;
+                    bytes_read_to_iov += read;
+                    if read < iov.iov_len {
+                        break;
+                    }
+                }
+            }
+        }
+
+        Ok(bytes_read_to_iov)
     }
 
     unsafe fn sendmsg(socket: c_int, msg: *const msghdr, flags: c_int) -> Result<usize> {
-        //TODO: implement sendto with sendmsg
-        eprintln!("sendmsg not implemented on redox");
-        Err(Errno(ENOSYS))
+        if msg.is_null() {
+            return Err(Errno(EINVAL));
+        }
+        let mhdr = &*msg;
+
+        let mut fds_to_send: Vec<FdGuard<c_int>> = Vec::new();
+        let mut ancillary_data_buffer: Vec<u8> = Vec::new();
+
+        // 1. Process Control Messages from msghdr and serialize them.
+        if mhdr.msg_controllen > 0 {
+            if mhdr.msg_control.is_null() {
+                return Err(Errno(EINVAL));
+            }
+
+            let mut cmsg: *mut cmsghdr = CMSG_FIRSTHDR(msg);
+            while !cmsg.is_null() {
+                let current_cmsg = &*cmsg;
+                if current_cmsg.cmsg_len < CMSG_ALIGN(mem::size_of::<cmsghdr>()) {
+                    return Err(Errno(EINVAL));
+                }
+
+                match (current_cmsg.cmsg_level, current_cmsg.cmsg_type) {
+                    (SOL_SOCKET, SCM_RIGHTS) => {
+                        let data_len =
+                            current_cmsg.cmsg_len - CMSG_ALIGN(mem::size_of::<cmsghdr>());
+                        if data_len % mem::size_of::<c_int>() != 0 {
+                            return Err(Errno(EINVAL));
+                        }
+                        let fd_count = data_len / mem::size_of::<c_int>();
+
+                        if fd_count > 0 {
+                            let fds_ptr = CMSG_DATA(cmsg) as *const c_int;
+                            let fds_slice = slice::from_raw_parts(fds_ptr, fd_count);
+                            fds_to_send.extend_from_slice(FdGuard::new(fds_slice));
+                        }
+
+                        // Serialize: [cmsg_level, cmsg_type, number of fds]
+                        ancillary_data_buffer
+                            .extend_from_slice(&current_cmsg.cmsg_level.to_le_bytes());
+                        ancillary_data_buffer
+                            .extend_from_slice(&current_cmsg.cmsg_type.to_le_bytes());
+                        ancillary_data_buffer.extend_from_slice(&(fd_count as u32).to_le_bytes());
+                    }
+                    (SOL_SOCKET, SCM_CREDENTIALS) => {
+                        // Serialize: [cmsg_level, cmsg_type]
+                        // The actual credentials will be fetched by the receiver from the kernel
+                        ancillary_data_buffer
+                            .extend_from_slice(&current_cmsg.cmsg_level.to_le_bytes());
+                        ancillary_data_buffer
+                            .extend_from_slice(&current_cmsg.cmsg_type.to_le_bytes());
+                    }
+                    _ => {
+                        eprintln!("sendmsg: Unsupported cmsg level or type");
+                        return Err(Errno(EOPNOTSUPP));
+                    }
+                }
+                cmsg = CMSG_NXTHDR(msg, cmsg);
+            }
+        }
+
+        // 2. Send FDs using the special syscall.
+        for &fd in &fds_to_send {
+            syscall::sendfd(socket as usize, *fd as usize)?;
+        }
+
+        // 3. Send the serialized ancillary data.
+        // 3.1. Send the size of the ancillary data buffer first.
+        let ancillary_len: usize = ancillary_data_buffer.len();
+        Self::write(socket, &ancillary_len.to_le_bytes())?;
+
+        // 3.2. Send the ancillary data buffer itself, if any.
+        if ancillary_len > 0 {
+            Self::write(socket, &ancillary_data_buffer)?;
+        }
+
+        // 4. Send the actual message data from iov.
+        let mut bytes_sent_from_iov = 0;
+        if mhdr.msg_iovlen > 0 {
+            let iovs = slice::from_raw_parts(mhdr.msg_iov, mhdr.msg_iovlen as usize);
+            for iov in iovs {
+                if !iov.iov_base.is_null() && iov.iov_len > 0 {
+                    let data_slice = slice::from_raw_parts(iov.iov_base as *const u8, iov.iov_len);
+                    let written = Self::write(socket, data_slice)?;
+                    bytes_sent_from_iov += written;
+                }
+            }
+        }
+
+        Ok(bytes_sent_from_iov)
     }
 
     unsafe fn sendto(
@@ -440,7 +670,7 @@ impl PalSocket for Sys {
             (AF_UNIX, SOCK_STREAM) => {
                 let listener = FdGuard::new(syscall::open("/scheme/uds_stream", flags | O_CREAT)?);
 
-                // For now, chan: lets connects be instant, and instead blocks
+                // For now, uds_stream: lets connects be instant, and instead blocks
                 // on any I/O performed. So we don't need to mark this as
                 // nonblocking.
 
@@ -455,7 +685,7 @@ impl PalSocket for Sys {
             (AF_UNIX, SOCK_DGRAM) => {
                 let listener = FdGuard::new(syscall::open("/scheme/uds_dgram", flags | O_CREAT)?);
 
-                // For now, chan: lets connects be instant, and instead blocks
+                // For now, uds_dgram: lets connects be instant, and instead blocks
                 // on any I/O performed. So we don't need to mark this as
                 // nonblocking.
 
