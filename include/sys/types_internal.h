2c4bd8d06182af7de0acb7707acbb475d15fbdf1
Wildan Mubarok <willnode@wellosoft.net>
2025-07-21 00:15:02 +0000

    Fix grp.h and gid_t/uid_t for Go

--- include/sys/types_internal.h
+++ include/sys/types_internal.h
@@ -5,8 +5,8 @@
 typedef long blksize_t;
 typedef long dev_t;
 typedef unsigned long ino_t;
-typedef int gid_t;
-typedef int uid_t;
+typedef unsigned int gid_t;
+typedef unsigned int uid_t;
 typedef int mode_t;
 typedef unsigned long nlink_t;
 typedef long long off_t;
