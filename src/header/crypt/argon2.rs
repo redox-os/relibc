b8598f50ff4c5e544ef05a87ccb5e792b280e556
Wildan M <willnode@wellosoft.net>
2025-09-19 15:41:05 +0700

    Add argon2 crypt

---
+++ src/header/crypt/argon2.rs
@@ -0,0 +1,16 @@
+use alloc::string::{String, ToString};
+use argon2::{
+    password_hash::{PasswordHash, PasswordVerifier},
+    Argon2,
+};
+
+pub fn crypt_argon2(key: &str, setting: &str) -> Option<String> {
+    let hash = PasswordHash::new(setting).ok()?;
+    let argon2 = Argon2::default();
+
+    if argon2.verify_password(key.as_bytes(), &hash).is_ok() {
+        Some(setting.to_string())
+    } else {
+        None
+    }
+}
