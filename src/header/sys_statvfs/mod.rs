0d34c229ff1b47b574983b61d0c33a3635924a48
auronandace <auronandace@googlemail.com>
2025-10-09 17:02:54 +0100

    update and add spec links

--- src/header/sys_statvfs/mod.rs
+++ src/header/sys_statvfs/mod.rs
@@ -1,4 +1,6 @@
-//! statvfs implementation for Redox, following http://pubs.opengroup.org/onlinepubs/7908799/xsh/sysstatvfs.h.html
+//! `sys_statvfs.h` implementation.
+//!
+//! See <https://pubs.opengroup.org/onlinepubs/9799919799/basedefs/sys_statvfs.h.html>.
 
 use crate::{
     c_str::CStr,
@@ -27,12 +29,14 @@ pub struct statvfs {
     pub f_namemax: c_ulong,
 }
 
+/// See <https://pubs.opengroup.org/onlinepubs/9799919799/functions/fstatvfs.html>.
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn fstatvfs(fildes: c_int, buf: *mut statvfs) -> c_int {
     let buf = Out::nonnull(buf);
     Sys::fstatvfs(fildes, buf).map(|()| 0).or_minus_one_errno()
 }
 
+/// See <https://pubs.opengroup.org/onlinepubs/9799919799/functions/fstatvfs.html>.
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn statvfs(file: *const c_char, buf: *mut statvfs) -> c_int {
     let file = CStr::from_ptr(file);
