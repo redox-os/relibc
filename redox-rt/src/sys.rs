c75b36d9de6964bfe2baae3b483d255441c46baf
Ibuki.O <isan.ethen@gmail.com>
2025-10-17 08:50:35 +0900

    fix: Fix typo.

--- redox-rt/src/sys.rs
+++ redox-rt/src/sys.rs
@@ -401,7 +401,7 @@ pub fn mkns(names: &[IoSlice]) -> Result<usize> {
 }
 pub fn register_scheme(name: &str, cap_fd: usize) -> Result<()> {
     const TYPE_REGISTER: usize = 2; // namespace dup type.
-    let mut buf = Vec::from(TYPE_MKNS.to_ne_bytes());
+    let mut buf = Vec::from(TYPE_REGISTER.to_ne_bytes());
     buf.extend_from_slice(name.as_bytes());
     let ns_this_scheme = syscall::dup(crate::current_namespace_fd(), buf)?;
     let mut cap_bytes = cap_fd.to_ne_bytes();
