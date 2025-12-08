7c80b1823059b9f419619861ca65107c2a7e1441
Ibuki.O <isan.ethen@gmail.com>
2025-12-08 16:14:41 +0900

    fix: Ignore object files that doesn't include rust mangled symbols.

--- renamesyms.sh
+++ renamesyms.sh
@@ -44,7 +44,7 @@ for special_sym in "${special_syms[@]}"; do
     echo "$special_sym __relibc_$special_sym" >> $symbols_file
 done
 
-mangled_alloc_syms=$("${NM}" --format=posix -g "$target" 2>/dev/null | awk '{print $1}' | grep "^_RNv" | grep -E "(___rust_|___rg_)")
+mangled_alloc_syms=$("${NM}" --format=posix -g "$target" 2>/dev/null | awk '{print $1}' | grep "^_RNv" | grep -E "(___rust_|___rg_)" || true)
 
 for sym in $mangled_alloc_syms; do
     echo "$sym __relibc_$sym" >> $symbols_file
