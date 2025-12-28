e0d41a17e7255ca742735b374b7ead74333a92c4
Wildan M <willnode@wellosoft.net>
2025-12-28 15:45:17 +0700

    Disable test requiring custom locales

--- tests/locale/newlocale.c
+++ tests/locale/newlocale.c
@@ -10,11 +10,9 @@ int main() {
     locale_t locale1 = newlocale(LC_ALL_MASK, "non-existent-locale", (locale_t)0);
     assert(locale1 == (locale_t)0);
 
-    // TODO: locale files inside redoxer?
-#ifndef __redox__
-    locale_t locale2 = newlocale(LC_ALL_MASK, "en_US", (locale_t)0);
-    assert(locale2 != (locale_t)0);
-#endif
+    // TODO: locale files inside redoxer (linux and redox)?
+    // locale_t locale2 = newlocale(LC_ALL_MASK, "en_US", (locale_t)0);
+    // assert(locale2 != (locale_t)0);
 
     return 0;
 }
