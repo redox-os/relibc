1e587dc77b42426f0ccac821ad355ed87ae0d400
Wildan M <willnode@wellosoft.net>
2026-01-05 12:43:51 +0700

    Add putc_unlocked tests

---
+++ tests/stdio/putc_unlocked.c
@@ -0,0 +1,24 @@
+#include <assert.h>
+#include <stdio.h>
+
+#include "test_helpers.h"
+
+int main(void)
+{
+    FILE* fp = tmpfile();
+    assert(fp != NULL);
+    flockfile(fp);
+    int c = 'c', r = 0;
+    r = putc_unlocked(c, fp);
+    ERROR_IF(putc_unlocked, r, == EOF);
+    // TODO: hang
+    // r = fflush(fp);
+    // ERROR_IF(fflush, r, == EOF);
+    funlockfile(fp);
+    // make sure unlock works
+    r = putc(c, fp);
+    ERROR_IF(putc, r, == EOF);
+    r = fflush(fp);
+    ERROR_IF(fflush, r, == EOF);
+    return 0;
+}
