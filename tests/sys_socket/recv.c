02752dda271202e05c1b059e25e6d6699470eee8
Wildan M <willnode@wellosoft.net>
2026-01-07 22:59:34 +0700

    Enable some tests

--- tests/sys_socket/recv.c
+++ tests/sys_socket/recv.c
@@ -20,7 +20,7 @@ int main(void)
     ERROR_IF(bind, status, == -1);
 
     status = listen(listen_fd, 1);
-    ERROR_IF(bind, status, == -1);
+    ERROR_IF(listen, status, == -1);
 
     status = getsockname(listen_fd, saddr, &addr_len);
     ERROR_IF(getsockname, status, == -1);
@@ -32,11 +32,11 @@ int main(void)
     ERROR_IF(connect, status, == -1);
 
     int server_fd = accept(listen_fd, NULL, NULL);
-    ERROR_IF(connect, status, == -1);
+    ERROR_IF(accept, status, == -1);
 
     char *c = "foo";
     status = send(server_fd, c, 4, 0);
-    ERROR_IF(sendto, status, == -1);
+    ERROR_IF(send, status, == -1);
 
     char x[4];
     ssize_t amount = recv(client_fd, x, 4, 0);
