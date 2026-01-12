8b5f89ab05d3fbea577ee67a73b0f76777d31f51
Pascal Reich <pascalr@andrew.cmu.edu>
2026-01-12 11:55:53 -0500

    add long double constants

--- include/math.h
+++ include/math.h
@@ -4,6 +4,8 @@
 typedef float float_t;
 typedef double double_t;
 
+/* double */
+
 #ifndef M_PI
 #define	M_PI		3.14159265358979323846	/* pi */
 #endif
@@ -70,4 +72,58 @@ typedef double double_t;
 
 #ifndef M_SQRT1_2
 #define M_SQRT1_2	0.70710678118654752440	/* 1/sqrt(2) */
+#endif
+
+/* long double */
+
+#ifndef M_El
+#define M_El        2.718281828459045235360287471352662498L /* e */
+#endif
+
+#ifndef M_LOG2El
+#define M_LOG2El    1.442695040888963407359924681001892137L /* log_2 e */
+#endif
+
+#ifndef M_LOG10El
+#define M_LOG10El   0.434294481903251827651128918916605082L /* log_10 e */
+#endif
+
+#ifndef M_LN2l
+#define M_LN2l      0.693147180559945309417232121458176568L /* log_e 2 */
+#endif
+
+#ifndef M_LN10l
+#define M_LN10l     2.302585092994045684017991454684364208L /* log_e 10 */
+#endif
+
+#ifndef M_PIl
+#define M_PIl       3.141592653589793238462643383279502884L /* pi */
+#endif
+
+#ifndef M_PI_2l
+#define M_PI_2l     1.570796326794896619231321691639751442L /* pi/2 */
+#endif
+
+#ifndef M_PI_4l
+#define M_PI_4l     0.785398163397448309615660845819875721L /* pi/4 */
+#endif
+
+#ifndef M_1_PIl
+#define M_1_PIl     0.318309886183790671537767526745028724L /* 1/pi */
+#endif
+
+#ifndef M_2_PIl
+#define M_2_PIl     0.636619772367581343075535053490057448L /* 2/pi */
+#endif
+
+#ifndef M_2_SQRTPIl
+#define M_2_SQRTPIl 1.128379167095512573896158903121545172L /* 2/sqrt(pi) */
+#endif
+
+#ifndef M_SQRT2l
+#define M_SQRT2l    1.414213562373095048801688724209698079L /* sqrt(2) */
+#endif
+
+#ifndef M_SQRT1_2l
+#define M_SQRT1_2l  0.707106781186547524400844362104849039L /* 1/sqrt(2) */
 #endif
