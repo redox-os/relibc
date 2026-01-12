08f93dfbdcd2d7d39c1fe92bd070063d47ac4beb
Pascal Reich <pascalreich44@gmail.com>
2026-01-12 19:38:28 +0000

    add mathematical constants

--- include/math.h
+++ include/math.h
@@ -4,6 +4,8 @@
 typedef float float_t;
 typedef double double_t;
 
+/* double */
+
 #ifndef M_PI
 #define	M_PI		3.14159265358979323846	/* pi */
 #endif
@@ -18,4 +20,94 @@ typedef double double_t;
 
 #ifndef M_2_PI
 #define M_2_PI		0.63661977236758134308	/* 2/pi */
+#endif
+
+#ifndef M_E
+#define M_E			2.7182818284590452354	/* e */
+#endif
+
+#ifndef M_LOG2E
+#define M_LOG2E		1.4426950408889634074	/* log_2 e */
+#endif
+
+#ifndef M_LOG10E
+#define M_LOG10E	0.43429448190325182765	/* log_10 e */
+#endif
+
+#ifndef M_LN2
+#define M_LN2		0.69314718055994530942	/* log_e 2 */
+#endif
+
+#ifndef M_LN10
+#define M_LN10		2.30258509299404568402	/* log_e 10 */
+#endif
+
+#ifndef M_1_PI
+#define M_1_PI		0.31830988618379067154	/* 1/pi */
+#endif
+
+#ifndef M_2_SQRTPI
+#define M_2_SQRTPI	1.12837916709551257390	/* 2/sqrt(pi) */
+#endif
+
+#ifndef M_SQRT2
+#define M_SQRT2		1.41421356237309504880	/* sqrt(2) */
+#endif
+
+#ifndef M_SQRT1_2
+#define M_SQRT1_2	0.70710678118654752440	/* 1/sqrt(2) */
+#endif
+
+/* long double */
+
+#ifndef M_El
+#define M_El        2.718281828459045235360287471352662498L /* e */
+#endif
+
+#ifndef M_LOG2El
+#define M_LOG2El    1.442695040888963407359924681001892137L /* log_2 e */
+#endif
+
+#ifndef M_LOG10El
+#define M_LOG10El   0.434294481903251827651128918916605082L /* log_10 e */
+#endif
+
+#ifndef M_LN2l
+#define M_LN2l      0.693147180559945309417232121458176568L /* log_e 2 */
+#endif
+
+#ifndef M_LN10l
+#define M_LN10l     2.302585092994045684017991454684364208L /* log_e 10 */
+#endif
+
+#ifndef M_PIl
+#define M_PIl       3.141592653589793238462643383279502884L /* pi */
+#endif
+
+#ifndef M_PI_2l
+#define M_PI_2l     1.570796326794896619231321691639751442L /* pi/2 */
+#endif
+
+#ifndef M_PI_4l
+#define M_PI_4l     0.785398163397448309615660845819875721L /* pi/4 */
+#endif
+
+#ifndef M_1_PIl
+#define M_1_PIl     0.318309886183790671537767526745028724L /* 1/pi */
+#endif
+
+#ifndef M_2_PIl
+#define M_2_PIl     0.636619772367581343075535053490057448L /* 2/pi */
+#endif
+
+#ifndef M_2_SQRTPIl
+#define M_2_SQRTPIl 1.128379167095512573896158903121545172L /* 2/sqrt(pi) */
+#endif
+
+#ifndef M_SQRT2l
+#define M_SQRT2l    1.414213562373095048801688724209698079L /* sqrt(2) */
+#endif
+
+#ifndef M_SQRT1_2l
+#define M_SQRT1_2l  0.707106781186547524400844362104849039L /* 1/sqrt(2) */
 #endif
