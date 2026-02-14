6ed46304c0c7ccd5d33f24c07774e3464116d1af
auronandace <auronandace@googlemail.com>
2026-02-14 16:57:53 +0000

    add annotation to redox function as exception

--- src/platform/redox/libredox.rs
+++ src/platform/redox/libredox.rs
@@ -255,11 +255,12 @@ pub unsafe extern "C" fn redox_get_ens_v0() -> RawResult {
 pub unsafe extern "C" fn redox_get_ns_v0() -> RawResult {
     Error::mux(redox_rt::sys::getns())
 }
+#[allow(improper_ctypes_definitions)]
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn redox_get_proc_credentials_v1(
     cap_fd: usize,
     target_pid: usize,
-    buf: &mut [u8],
+    buf: &mut [u8], // not FFI safe
 ) -> RawResult {
     Error::mux(redox_rt::sys::get_proc_credentials(cap_fd, target_pid, buf))
 }
