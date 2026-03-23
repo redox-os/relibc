523086be9e92041f392747ac569e22a2d729d318
Zero <khaled.alam5602@gmail.com>
2026-03-24 01:40:58 +0200

    feat(time): Add process_pid to timer_internal_t
    Add `process_pid` field to `timer_internal_t` struct.
    This field is used by the `alarm()` function to specify the PID of the process
    to which the `SIGALRM` signal should be delivered.

--- src/platform/pal/signal.rs
+++ src/platform/pal/signal.rs
@@ -4,8 +4,9 @@ use crate::{
     header::{
         bits_time::timespec,
         signal::{sigaction, siginfo_t, sigset_t, sigval, stack_t},
-        sys_time::itimerval,
+        sys_time::{self, itimerval},
     },
+    platform,
 };
 
 pub trait PalSignal: Pal {
@@ -20,6 +21,32 @@ pub trait PalSignal: Pal {
     fn raise(sig: c_int) -> Result<()>;
 
     fn setitimer(which: c_int, new: &itimerval, old: Option<&mut itimerval>) -> Result<()>;
+
+    /// See <https://pubs.opengroup.org/onlinepubs/9799919799/functions/alarm.html>.
+    ///
+    /// Default implementation uses setitimer(ITIMER_REAL). Platforms that do not
+    /// support setitimer (e.g. Redox) must override this.
+    fn alarm(seconds: c_uint) -> c_uint {
+        use crate::header::sys_select::timeval;
+
+        let timer = itimerval {
+            it_value: timeval {
+                tv_sec: time_t::from(seconds),
+                tv_usec: 0,
+            },
+            ..Default::default()
+        };
+        let mut otimer = itimerval::default();
+
+        let errno_backup = platform::ERRNO.get();
+        let secs = if Self::setitimer(sys_time::ITIMER_REAL, &timer, Some(&mut otimer)).is_err() {
+            0
+        } else {
+            otimer.it_value.tv_sec as c_uint + if otimer.it_value.tv_usec > 0 { 1 } else { 0 }
+        };
+        platform::ERRNO.set(errno_backup);
+        secs
+    }
 
     fn sigaction(sig: c_int, act: Option<&sigaction>, oact: Option<&mut sigaction>) -> Result<()>;
 
