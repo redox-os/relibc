7ade6fc0cacdd53571b92e961875668621dea23f
Connor-GH <connor-gh@outlook.com>
2026-04-13 19:59:38 -0500

    limits.h: add more constants
    This helps us to pass several tests in Sortie's os-test suite. I forget
    the exact number, but it was something like 8 or so tests:
    https://doc.redox-os.org/test-results/os-test/os-test-20260410/html/limits/index.html#limits

--- src/header/unistd/pathconf.rs
+++ src/header/unistd/pathconf.rs
@@ -1,5 +1,12 @@
 use crate::{
-    header::{errno, termios::_POSIX_VDISABLE},
+    header::{
+        errno,
+        limits::{
+            FILESIZEBITS, LINK_MAX, MAX_CANON, MAX_INPUT, NAME_MAX, PATH_MAX, PIPE_BUF,
+            POSIX_ALLOC_SIZE_MIN, SYMLINK_MAX,
+        },
+        termios::_POSIX_VDISABLE,
+    },
     platform::{
         self,
         types::{c_char, c_int, c_long},
@@ -32,12 +39,12 @@ pub const _PC_2_SYMLINKS: c_int = 20;
 fn pc(name: c_int) -> c_long {
     // Settings from musl, some adjusted
     match name {
-        _PC_LINK_MAX => 127,
-        _PC_MAX_CANON => 255,
-        _PC_MAX_INPUT => 255,
-        _PC_NAME_MAX => 255,
-        _PC_PATH_MAX => 4096,
-        _PC_PIPE_BUF => 4096,
+        _PC_LINK_MAX => LINK_MAX.try_into().unwrap_or(-1),
+        _PC_MAX_CANON => MAX_CANON.try_into().unwrap_or(-1),
+        _PC_MAX_INPUT => MAX_INPUT.try_into().unwrap_or(-1),
+        _PC_NAME_MAX => NAME_MAX.try_into().unwrap_or(-1),
+        _PC_PATH_MAX => PATH_MAX.try_into().unwrap_or(-1),
+        _PC_PIPE_BUF => PIPE_BUF.try_into().unwrap_or(-1),
         _PC_CHOWN_RESTRICTED => 1,
         _PC_NO_TRUNC => 1,
         _PC_VDISABLE => _POSIX_VDISABLE.into(),
@@ -45,13 +52,13 @@ fn pc(name: c_int) -> c_long {
         _PC_ASYNC_IO => -1,
         _PC_PRIO_IO => -1,
         _PC_SOCK_MAXBUF => -1,
-        _PC_FILESIZEBITS => 64,
+        _PC_FILESIZEBITS => FILESIZEBITS.into(),
         _PC_REC_INCR_XFER_SIZE => -1,
         _PC_REC_MAX_XFER_SIZE => -1,
         _PC_REC_MIN_XFER_SIZE => 4096,
         _PC_REC_XFER_ALIGN => 4096,
-        _PC_ALLOC_SIZE_MIN => 4096,
-        _PC_SYMLINK_MAX => -1,
+        _PC_ALLOC_SIZE_MIN => POSIX_ALLOC_SIZE_MIN.try_into().unwrap_or(-1),
+        _PC_SYMLINK_MAX => SYMLINK_MAX.try_into().unwrap_or(-1),
         _PC_2_SYMLINKS => 1,
         _ => {
             platform::ERRNO.set(errno::EINVAL);
