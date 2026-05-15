0489f5ef442a1061de42d385690b2e39f9525712
auronandace <auronandace@googlemail.com>
2026-05-15 11:27:07 +0100

    remove sys_types from sys_times includes

--- src/header/sys_times/mod.rs
+++ src/header/sys_times/mod.rs
@@ -2,10 +2,10 @@
 //!
 //! See <https://pubs.opengroup.org/onlinepubs/9799919799/basedefs/sys_times.h.html>.
 
-use crate::platform;
-use platform::{Pal, Sys, types::clock_t};
+use crate::platform::types::clock_t;
 
 /// See <https://pubs.opengroup.org/onlinepubs/9799919799/basedefs/sys_times.h.html>.
+#[allow(non_camel_case_types)]
 #[repr(C)]
 pub struct tms {
     tms_utime: clock_t,
@@ -17,5 +17,5 @@ pub struct tms {
 /// See <https://pubs.opengroup.org/onlinepubs/9799919799/functions/times.html>.
 #[unsafe(no_mangle)]
 pub extern "C" fn times(out: *mut tms) -> clock_t {
-    Sys::times(out)
+    todo_panic!(0, "TODO: times not implemented");
 }
