dbe84f1503d380c86d1c9d3ad0c5d2a10ad64b72
auronandace <auronandace@googlemail.com>
2026-05-16 07:59:43 +0100

    split out nlink_t from sys_types header

--- src/header/mod.rs
+++ src/header/mod.rs
@@ -22,6 +22,8 @@ pub mod bits_iovec;
 pub mod bits_locale_t;
 #[path = "bits_mode-t/mod.rs"]
 pub mod bits_mode_t;
+#[path = "bits_nlink-t/mod.rs"]
+pub mod bits_nlink_t;
 #[path = "bits_off-t/mod.rs"]
 pub mod bits_off_t;
 #[path = "bits_pid-t/mod.rs"]
