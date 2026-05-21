4341a11f51bfe9174acc40c1ebeec9e18d6e3a0e
auronandace <auronandace@googlemail.com>
2026-05-21 12:30:13 +0100

    clean up sys_syscall header

--- src/header/sys_syscall/mod.rs
+++ src/header/sys_syscall/mod.rs
@@ -1,3 +1,7 @@
+//! `sys/syscall.h` implementation.
+//!
+//! Non-POSIX, see <https://www.man7.org/linux/man-pages/man2/syscall.2.html>.
+
 // copied from sc crate
 #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
 pub mod aarch64;
