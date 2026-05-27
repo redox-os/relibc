ced2dc8965a4af2e502c2d9781500eaf254d75bf
auronandace <auronandace@googlemail.com>
2026-05-27 09:01:48 +0100

    testing removal of extra types from sys_types

--- src/header/sys_types_internal/mod.rs
+++ src/header/sys_types_internal/mod.rs
@@ -32,15 +32,3 @@ pub use crate::header::{
     bits_uid_t::uid_t,
 };
 use crate::platform::types::{c_char, c_longlong, c_uchar, c_uint, c_ulong, c_ulonglong, c_ushort};
-
-pub type u_char = c_uchar;
-pub type uchar = c_uchar;
-pub type u_short = c_ushort;
-pub type ushort = c_ushort;
-pub type u_int = c_uint;
-pub type uint = c_uint;
-pub type u_long = c_ulong;
-pub type ulong = c_ulong;
-pub type quad_t = c_longlong;
-pub type u_quad_t = c_ulonglong;
-pub type caddr_t = *mut c_char;
