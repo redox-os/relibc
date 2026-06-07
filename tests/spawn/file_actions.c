0d127d3652de622550911fc3d2776183349f36c7
R Aadarsh <raadarsh1107@outlook.com>
2026-06-07 12:49:39 +0530

    Add more tests

--- tests/spawn/file_actions.c
+++ tests/spawn/file_actions.c
@@ -1,8 +1,8 @@
+#include "../test_helpers.h"
 #include <assert.h>
 #include <dirent.h>
-#include <errno.h>
 #include <fcntl.h>
-#include <linux/limits.h>
+#include <limits.h>
 #include <sched.h>
 #include <spawn.h>
 #include <stdio.h>
@@ -12,14 +12,8 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define ERROR_IF(func, status, condition)                                      \
-  do {                                                                         \
-    if (status condition) {                                                    \
-      fprintf(stderr, "%s:%s:%d: '%s' failed: %s (%d)\n", __FILE__, __func__,  \
-              __LINE__, #func, strerror(errno), errno);                        \
-      _exit(EXIT_FAILURE);                                                     \
-    }                                                                          \
-  } while (0)
+static const char buf[] = "#include <unistd.h>\nint main()\n{\nwrite(900, "
+                          "\"HELLO REDOX\\n\", 12);\n}";
 
 #define OPTIONALLY_ERROR(function, args, cond)                                 \
   {                                                                            \
@@ -72,4 +66,29 @@ int main() {
   ERROR_IF(unlink, status, != 0);
   status = rmdir("./hello");
   ERROR_IF(rmdir, status, != 0);
+
+  // TEST: dup2
+  pid = 0;
+  posix_spawn_file_actions_init(&fa);
+  posix_spawn_file_actions_adddup2(&fa, 1, 900);
+
+  FILE *f = fopen("./dup2_check.c", "w");
+  if (!f) {
+    printf("Failed to create dup2_check.c\n");
+    exit(EXIT_FAILURE);
+  }
+  OPTIONALLY_ERROR(fwrite, (buf, sizeof(char), 67, f), == 67);
+  fclose(f);
+  char *argv2[] = {"gcc", "./dup2_check.c", "-o", "d.out", NULL};
+  OPTIONALLY_ERROR(posix_spawnp, (&pid, "gcc", NULL, NULL, argv2, environ),
+                   == 0);
+  waitpid(pid, NULL, 0);
+  assert(pid != 0);
+  pid = 0;
+  char *argv3[] = {"./d.out", NULL};
+  OPTIONALLY_ERROR(posix_spawnp, (&pid, "./d.out", &fa, NULL, argv3, environ),
+                   == 0);
+  assert(pid != 0);
+  waitpid(pid, NULL, 0);
+  posix_spawn_file_actions_destroy(&fa);
 }
