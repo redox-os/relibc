3be84f4b26109309b795899d7ab3f466ebc1dc43
auronandace <auronandace@googlemail.com>
2026-06-24 11:52:15 +0100

    move stddef header from C to cbindgen

--- src/header/bits_size-t/mod.rs
+++ src/header/bits_size-t/mod.rs
@@ -1,3 +1,7 @@
+//! `size_t` from `stddef.h` implementation.
+//!
+//! See <https://pubs.opengroup.org/onlinepubs/9799919799/basedefs/stddef.h.html>.
+
 use crate::platform::types::c_ulong;
 
 /// Unsigned integer type of the result of the sizeof operator.
