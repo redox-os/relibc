bbac94480b6ab1b8721b4a73229193c6ce1450c2
auronandace <auronandace@googlemail.com>
2026-06-25 11:26:36 +0100

    add descriptions to sys_epoll functions

--- src/header/bits_sigset-t/mod.rs
+++ src/header/bits_sigset-t/mod.rs
@@ -1,3 +1,7 @@
+//! `sigset_t` for `signal.h` implementation.
+//!
+//! See <https://pubs.opengroup.org/onlinepubs/9799919799/basedefs/signal.h.html>.
+
 use crate::platform::types::c_ulonglong;
 
 /// Integer type of an object used to represent sets of signals.
