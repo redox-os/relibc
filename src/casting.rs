f99fcab2ae2e6595a1a3b24bf2c9632ed686eaf9
auronandace <auronandace@googlemail.com>
2026-07-20 11:25:16 +0100

    qualify method

--- src/casting.rs
+++ src/casting.rs
@@ -50,7 +50,7 @@ impl CCharPtr {
         }
         #[cfg(any(target_arch = "aarch64", target_arch = "riscv64"))]
         {
-            return input.cast_const();
+            return core::ptr::cast_const::<c_char>(input);
         }
         panic!("Arch not supported!")
     }
